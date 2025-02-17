f48f8a1f34097b1da6a3b0514dd5021cde8da042
Kalicz Péter <kaliczp@gmail.com>
2025-02-17 07:59:21 +0100

    All columnt in one pdf with zoo

--- Import.R
+++ Import.R
@@ -5,5 +5,12 @@ Sys.setenv(TZ = "UTC")
 all.xts <- xts(as.matrix(raw[,2:ncol(raw)]), raw[,1,drop = TRUE])
 
 ## Watch
-Oszlop <- 1
 plot(all.xts[, Oszlop], main = colnames(all.xts)[Oszlop])
+
+## All in one file with zoo
+pdf("Mind.pdf", widt = 14)
+for(Oszlop in 1:ncol(all.xts)){
+    plot.zoo(all.xts[, Oszlop], ylab = colnames(all.xts)[Oszlop],
+             xaxs = "i", xlab = "")
+}
+dev.off()
